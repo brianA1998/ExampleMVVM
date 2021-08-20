6c0eaa44fa6d4bdb2b5e66f02cdd1211e0f1d603
brianA1998 <brian.alaniz@hotmail.com>
2021-08-20 18:03:50 -0300

    Terminando de configurar el viewmodel con el activity

--- app/src/main/java/com/example/examplemvvm/viewmodel/QuoteViewModel.kt
+++ app/src/main/java/com/example/examplemvvm/viewmodel/QuoteViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.examplemvvm.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.examplemvvm.model.QuoteModel
 import com.example.examplemvvm.model.QuoteProvider
